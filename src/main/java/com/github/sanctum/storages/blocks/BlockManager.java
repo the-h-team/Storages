8c97674d68e7a25f1453d3ef3bbdfc5f2ca1b9fe
Matt <58913953+ms5984@users.noreply.github.com>
2021-06-28 02:28:40 -0400

    * BlockManager needed details + flyweight factory

--- src/main/java/com/github/sanctum/storages/blocks/BlockManager.java
+++ src/main/java/com/github/sanctum/storages/blocks/BlockManager.java
@@ -1,23 +1,44 @@
 package com.github.sanctum.storages.blocks;
 
+import org.bukkit.block.Block;
 import org.bukkit.block.Container;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class BlockManager {
-    private final AtomicReference<Container> container = new AtomicReference<>();
+    private static final Map<BlockLocation, BlockManager> INSTANCES = new ConcurrentHashMap<>();
+    private final BlockLocation blockLocation;
+
+    private BlockManager(BlockLocation blockLocation) {
+        this.blockLocation = blockLocation;
+    }
 
     public <R> R queryContainer(Function<Container, R> queryFunction) {
         return queryFunction.apply(getContainer());
     }
 
     public void updateContainer(Consumer<Container> queryFunction) {
-        queryFunction.accept(getContainer());
+        queryFunction.andThen(Container::update).accept(getContainer());
     }
 
-    private Container getContainer() {
-        return container.get(); // TODO: update this reference
+    private Container getContainer() throws IllegalStateException {
+        final Container c;
+        try {
+            c = (Container) blockLocation.toBlock().getState();
+        } catch (ClassCastException e) {
+            throw new IllegalStateException(e);
+        }
+        return c;
+    }
+
+    public static BlockManager ofBlock(Block block) throws IllegalArgumentException {
+        final BlockLocation blockLocation = BlockLocation.of(block);
+        BlockManager blockManager = INSTANCES.get(blockLocation);
+        if (blockManager != null) return blockManager;
+        INSTANCES.put(blockLocation, (blockManager = new BlockManager(blockLocation)));
+        return blockManager;
     }
 }
